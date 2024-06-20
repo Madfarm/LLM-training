19718c6e41dde39c8789f07517d51a9160787a6a
AnthonyScott <anthony.scott1005@gmail.com>
2024-06-19 23:23:30 -0400

    Add setup for Gene dataset

---
+++ code_execution_file_upload/model_generated_code/gene_dataset/settings.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.insert(1, '/root/code-sandbox/LLM-training/code_execution_file_upload/model_generated_code/gene_dataset')
+from settings import file_path
+
+file_path = "/root/code-sandbox/LLM-training/code_execution_file_upload/tested_datasets/gene_data.csv"
